7f5d3264b0d1314f812b7bb4bf33487c9cbe5f00
Dudes.co <Dudes.co@DESKTOP-9GI0D3G>
2023-06-01 11:22:09 +0530

    1st commit.

---
+++ Doitnowdontbelate/src/test/java/practice/Script1Test.java
@@ -0,0 +1,40 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Script1Test
+{
+	
+	public void test1()
+	{
+		System.out.println("run1");
+	}
+	public void test2()
+	{
+		System.out.println("run1");
+	}
+
+
+	public void test31()
+	{
+		System.out.println("run1");
+	}
+
+
+	@Test
+	public void test3()
+	{
+		System.out.println("run3");
+	}
+	
+     @Test
+	public void test4()
+	{
+		System.out.println("run4");
+	}
+	
+	//hello how u douing
+	//hello how u douing
+
+
+}
---
+++ Doitnowdontbelate/src/test/java/simple/Scripting1Test.java
@@ -0,0 +1,16 @@
+package simple;
+
+import org.testng.annotations.Test;
+
+public class Scripting1Test {
+	
+	
+		@Test
+		public void demo1()
+		{
+			System.out.println("demo1");
+		}
+}
+
+
+//do it now
